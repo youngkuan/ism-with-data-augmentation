d9de2423b9e1f870c598e654434a78da409ac106
KuanYangGH <ykxjtuedu@163.com>
2019-03-18 13:37:48 +0800

    完善代码：补全模型权重初始化

--- modules/upsample_text2image.py
+++ modules/upsample_text2image.py
@@ -57,6 +57,10 @@ class UpsampleNetwork(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, 0.0, 0.02)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
